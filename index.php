8cdb19b036f929ddfb050fdcad6c3a2e63959795
Franco Cruz <fismaelcruzlopez@gmail.com>
2023-08-16 19:41:43 -0300

    agregado

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+class Persona{
+private int $id;
+private string $nombre;
+public function getNombre($id , $nombre){
+    $this->$nombre;
+    $this->$id;
+}
+}
+class casa{
+    private int $id;
+    private string $calle;
+    private int $numero;
+public function getCalle($calle){
+    $this->$calle;
+}
+}
+class
+ ;?>
